cdcee0bb5b7a8d59ce7846f3d7737b6bf5173cad
pulipakaa24 <pulipakaaditya@gmail.com>
2022-12-10 14:40:49 -0600

    changed name to make it easier for move forward simple auton

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton/MoveForward.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton/MoveForward.java
@@ -1,14 +1,9 @@
 package org.firstinspires.ftc.teamcode.Auton;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.teamcode.Hardware.Hardware;
-
-@Autonomous(name = "EncoderTestAditya", group = "Robot")
-public class EncoderTestAditya extends IterativeAuton {
+@Autonomous(name = "MoveForward", group = "Robot")
+public class MoveForward extends IterativeAuton {
     @Override
     public void start() {
         runtime.reset();
@@ -24,12 +19,12 @@ public class EncoderTestAditya extends IterativeAuton {
         int bLorig = robot.bLMotor.getCurrentPosition();
 
         robot.setDriveModeEncoder();
-        robot.fRMotor.setTargetPosition(1000);
-        robot.fLMotor.setTargetPosition(1000);
-        robot.bRMotor.setTargetPosition(1000);
-        robot.bLMotor.setTargetPosition(1000);
+        robot.fRMotor.setTargetPosition(-1000);
+        robot.fLMotor.setTargetPosition(-1000);
+        robot.bRMotor.setTargetPosition(-1000);
+        robot.bLMotor.setTargetPosition(-1000);
         robot.setDriveModeRTP();
-        robot.setDrivePower(-0.8, -0.8, -0.8, -0.8);
+        robot.setDrivePower(0.8, 0.8, 0.8, 0.8);
 
         while (true) {
             telemetry.addData("FR", fRorig);
