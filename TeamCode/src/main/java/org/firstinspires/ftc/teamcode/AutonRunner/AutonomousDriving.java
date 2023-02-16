130b75d8fe8b482fd1f97c931250d1ef7841a948
theKevKev <kevinhan.119@gmail.com>
2023-02-16 17:00:41 -0600

    MeepMepp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonRunner/AutonomousDriving.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonRunner/AutonomousDriving.java
@@ -5,5 +5,5 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import org.firstinspires.ftc.teamcode.Roadrunner.drive.SampleMecanumDrive;
 
 public abstract class AutonomousDriving extends LinearOpMode {
-    SampleMecanumDrive robot = new SampleMecanumDrive(hardwareMap);
+//    SampleMecanumDrive robot = new SampleMecanumDrive(hardwareMap);
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonRunner/LeftStackRunner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonRunner/LeftStackRunner.java
@@ -7,6 +7,8 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.teamcode.Auton.AutonDriving;
+import org.firstinspires.ftc.teamcode.Hardware.Hardware;
 import org.firstinspires.ftc.teamcode.Roadrunner.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.Roadrunner.trajectorysequence.TrajectorySequence;
 
@@ -14,13 +16,18 @@ import org.firstinspires.ftc.teamcode.Roadrunner.trajectorysequence.TrajectorySe
 public class LeftStackRunner extends AutonomousDriving {
     @Override
     public void runOpMode() throws InterruptedException {
-        Trajectory track1 = robot.trajectoryBuilder(new Pose2d(), true)
-                .strafeLeft(41.75)
+        SampleMecanumDrive robot = new SampleMecanumDrive(hardwareMap);
+
+        robot.setPoseEstimate(new Pose2d(-36,-65.75,0));
+
+        Trajectory track1 = robot.trajectoryBuilder(new Pose2d(-36, -65.75,0), true)
+                .addDisplacementMarker(() -> {
+                    // Lift to high preset
+                })
+                .strafeLeft(47.75)
                 .splineToSplineHeading(new Pose2d(-26.8,-2.8,Math.toRadians(225)), Math.toRadians(45))
-                .addTemporalMarker(0,() -> {
-                    //
-                    telemetry.addData("let's a go", "here");
-                    telemetry.update();
+                .addDisplacementMarker(() -> {
+                    // downDropUp
                 })
                 .build();
 
@@ -29,5 +36,7 @@ public class LeftStackRunner extends AutonomousDriving {
         if(isStopRequested()) return;
 
         robot.followTrajectory(track1);
+
+        sleep(10000);
     }
 }
